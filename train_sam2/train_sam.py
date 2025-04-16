5bc06146b6101fcd5a7fe92a297a396364ff323d
Ubuntu <ubuntu@ip-10-35-126-33.us-west-2.compute.internal>
2025-04-16 18:21:04 +0000

    testing 123

---
+++ train_sam2/train_sam.py
@@ -0,0 +1,139 @@
+import os
+# if using Apple MPS, fall back to CPU for unsupported ops
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+
+# select the device for computation
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"using device: {device}")
+
+if device.type == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+elif device.type == "mps":
+    print(
+        "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+        "give numerically different outputs and sometimes degraded performance on MPS. "
+        "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+    )
+
+
+
+data_dir=r"../../../data/davis-2017/DAVIS/" # Path to dataset (LabPics 1)
+data=[] # list of files in dataset
+for ff, name in enumerate(os.listdir(data_dir+"JPEGImages/480p/bear/")):  # go over all folder annotation
+    data.append({"image":data_dir+"JPEGImages/480p/bear/"+name,"annotation":data_dir+"Annotations/480p/bear/"+name[:-4]+".png"})
+
+
+
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+print(os.getcwd())
+
+sam2_checkpoint = "../checkpoints/sam2.1_hiera_large.pt"
+model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+
+sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+
+predictor = SAM2ImagePredictor(sam2_model)
+
+
+def read_batch(data): # read random image and its annotaion from  the dataset (LabPics)
+
+   #  select image
+
+        ent  = data[np.random.randint(len(data))] # choose random entry
+        Img = cv2.imread(ent["image"])[...,::-1]  # read image
+        ann_map = cv2.imread(ent["annotation"]) # read annotation
+
+   # resize image
+
+        r = np.min([1024 / Img.shape[1], 1024 / Img.shape[0]]) # scalling factor
+        Img = cv2.resize(Img, (int(Img.shape[1] * r), int(Img.shape[0] * r)))
+        ann_map = cv2.resize(ann_map, (int(ann_map.shape[1] * r), int(ann_map.shape[0] * r)),interpolation=cv2.INTER_NEAREST)
+
+   # merge vessels and materials annotations
+
+        mat_map = ann_map[:,:,0] # material annotation map
+        ves_map = ann_map[:,:,2] # vessel  annotaion map
+        mat_map[mat_map==0] = ves_map[mat_map==0]*(mat_map.max()+1) # merge maps
+
+   # Get binary masks and points
+
+        inds = np.unique(mat_map)[1:] # load all indices
+        points= []
+        masks = []
+        for ind in inds:
+            mask=(mat_map == ind).astype(np.uint8) # make binary mask corresponding to index ind
+            masks.append(mask)
+            coords = np.argwhere(mask > 0) # get all coordinates in mask
+            yx = np.array(coords[np.random.randint(len(coords))]) # choose random point/coordinate
+            points.append([[yx[1], yx[0]]])
+        return Img,np.array(masks),np.array(points), np.ones([len(masks),1])
+
+
+
+optimizer=torch.optim.AdamW(params=predictor.model.parameters(),lr=1e-5,weight_decay=4e-5)
+scaler = torch.cuda.amp.GradScaler() # mixed precision
+
+# Training loop
+
+for itr in range(10):
+    with torch.cuda.amp.autocast(): # cast to mix precision
+            image,mask,input_point, input_label = read_batch(data) # load data batch
+            if mask.shape[0]==0: continue # ignore empty batches
+            predictor.set_image(image) # apply SAM image encoder to the image
+
+            # prompt encoding
+
+            mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
+            sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels),boxes=None,masks=None,)
+
+            # mask decoder
+
+            batched_mode = unnorm_coords.shape[0] > 1 # multi object prediction
+            high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
+            low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(image_embeddings=predictor._features["image_embed"][-1].unsqueeze(0),image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),sparse_prompt_embeddings=sparse_embeddings,dense_prompt_embeddings=dense_embeddings,multimask_output=True,repeat_image=batched_mode,high_res_features=high_res_features,)
+            prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])# Upscale the masks to the original image resolution
+
+            # Segmentaion Loss caclulation
+
+            gt_mask = torch.tensor(mask.astype(np.float32)).cuda()
+            prd_mask = torch.sigmoid(prd_masks[:, 0])# Turn logit map to probability map
+            seg_loss = (-gt_mask * torch.log(prd_mask + 0.00001) - (1 - gt_mask) * torch.log((1 - prd_mask) + 0.00001)).mean() # cross entropy loss
+
+            # Score loss calculation (intersection over union) IOU
+
+            inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
+            iou = inter / (gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter)
+            score_loss = torch.abs(prd_scores[:, 0] - iou).mean()
+            loss=seg_loss+score_loss*0.05  # mix losses
+
+            # apply back propogation
+
+            predictor.model.zero_grad() # empty gradient
+            scaler.scale(loss).backward()  # Backpropogate
+            scaler.step(optimizer)
+            scaler.update() # Mix precision
+
+            if itr%1000==0: torch.save(predictor.model.state_dict(), "model.torch");print("save model")
+
+            # Display results
+
+            if itr==0: mean_iou=0
+            mean_iou = mean_iou * 0.99 + 0.01 * np.mean(iou.cpu().detach().numpy())
+            print("step)",itr, "Accuracy(IOU)=",mean_iou)

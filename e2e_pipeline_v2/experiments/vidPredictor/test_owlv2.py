b03e1442b944233754842876dc0c63c63cf794b3
Ubuntu <ubuntu@ip-10-35-126-83.us-west-2.compute.internal>
2025-05-06 20:34:52 +0000

    changed path

--- e2e_pipeline_v2/experiments/vidPredictor/test_owlv2.py
+++ e2e_pipeline_v2/experiments/vidPredictor/test_owlv2.py
@@ -3,9 +3,9 @@ from src.frame_loader import load_frames_from_directory
 from src.owlv2_detector import OWLv2Detector
 
 # Load a frame
-frames, _ = load_frames_from_directory("./video_frames")
+frames, _ = load_frames_from_directory("/home/ubuntu/code/drew/test_data/frames/Scenes 001-020__220D-2-_20230815190723523/subset")
 detector = OWLv2Detector()
 
 # Test on a single frame
-detections = detector.detect(frames[0], ["person", "chair", "table"])
+detections = detector.detect(frames[0], ["goat"])
 detector.visualize_detections(frames[0], detections)

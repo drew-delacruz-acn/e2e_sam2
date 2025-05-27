46f434c519db6b003927e073b0858be1746fd07f
Ubuntu <ubuntu@ip-10-35-126-83.us-west-2.compute.internal>
2025-05-27 19:54:38 +0000

    logs

---
+++ e2e_pipeline_v2/experiments/vidPredictor/contrastive_learning_v2/load_data.py
@@ -0,0 +1,44 @@
+import pickle 
+import pandas as pd
+from collections import Counter
+import numpy as np
+def load_pickle(file_path):
+    """
+    Load a pickle file and return the data.
+    
+    Args:
+        file_path (str): Path to the pickle file.
+        
+    Returns:
+        data: Data loaded from the pickle file.
+    """
+    with open(file_path, 'rb') as f:
+        data = pickle.load(f)
+        print(f"Loaded {data} from {file_path}")
+    
+
+    # Assuming `data` is the loaded dictionary
+    # representatives = data['representatives']       # Shape: (18, 2048)
+    # class_names = data['class_names']               # List of 18 class names
+    # # Create the DataFrame with class names and their representative embeddings
+    # # Ensure each embedding is explicitly np.float32
+    # embeddings = [np.array(vec, dtype=np.float32) for vec in representatives]
+
+    # # Create the DataFrame
+    # df = pd.DataFrame({
+    #     'class_name': class_names,
+    #     'embedding': embeddings
+    # })
+
+
+    # # Optionally, name the columns for clarity (e.g., feature_0, feature_1, ..., feature_2047)
+    # df.columns = [f'feature_{i}' for i in range(representatives.shape[1])]
+
+    return data  # Save to CSV
+
+
+df =load_pickle('scripts/high_confidence_fps.pkl')
+df.to_csv('false_positives.csv', index=False)
+# print(type(df['embedding'][0]), df['embedding'][0].dtype)
+# 
+
